b5e727e3dc2e1bc4f1dd58ed5bf7fc0c932baf8b
Pankaj Tiwary <pktiwary@gmail.com>
2015-09-25 11:20:03 -0400

    Added HelloWorld and jetty projects, modified README.md with description

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bigdata-training
+
+HelloWorld - A simple Hello World project in java
+jetty - A demo servlet that prints welcome message using jetty
---
+++ jetty/src/main/java/Servlet1.java
@@ -0,0 +1,40 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Servlet1() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>Hello Servlet</h1>");
+	}
+
+}
